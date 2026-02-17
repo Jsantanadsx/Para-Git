80c4626e36c000dd1f0b11748b57d950a08dda4e
Jsantanads <jvinajoza@gmail.com>
2026-02-17 13:10:26 -0300

    Sistema simples de Semáforo.

---
+++ Semáforo/Semaforo.py
@@ -0,0 +1,11 @@
+import time
+
+while True:
+    print("🟢 VERDE - Avance")
+    time.sleep(5)
+
+    print("🟡 AMARELO - Atenção")
+    time.sleep(2)
+
+    print("🔴 VERMELHO - Pare")
+    time.sleep(5)
