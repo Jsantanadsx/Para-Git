956a9b99ba6299ac63c6cd06674604c2cf7d00a0
Jsantanads <jvinajoza@gmail.com>
2026-02-17 12:33:56 -0300

    Comentando o código.

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,22 +1,34 @@
 resultado_atual = None
 
+#Loop : Calculadora só é interrompida quando o usuário digitar "S".
 while True:
+
+    # Se a variável não possui valor, "Digite o número inicial."
     if resultado_atual is None:
+        # Pede e converte o número inicial para float (decimal)
         resultado_atual = float(input("Digite o número inicial: "))
 
+    # Operação desejada 
+    # .upper() transforma a digitação em maiúscula (c para C, s para S) Padronizando.
     operacao = input(
         "Digite a operação (+, -, *, /), C para limpar, ou S para sair: "
     ).upper()
 
+    # Se o usuário digitou C (clear)
     if operacao == "C":
+        # Zera a memória
         resultado_atual = None
         print("Calculadora limpa.")
+        # Retorna ao início do loop
         continue
 
+    # Se o usuário digitou S (sair)
     if operacao == "S":
         print("Encerrando a calculadora...")
+        # Encerra o loop
         break
 
+    # Pede o próximo número para a operação
     num = float(input("Digite o próximo número: "))
 
     if operacao == "+":
@@ -29,14 +41,19 @@ while True:
         resultado_atual *= num
 
     elif operacao == "/":
+        # Verifica se não é divisão por zero
         if num != 0:
             resultado_atual /= num
         else:
             print("Erro: divisão por zero.")
+            # Retorna sem atualizar o resultado
             continue
-
+    
+    # Caso digite algo inválido
     else:
         print("Operação inválida.")
+        # Retorna ao loop
         continue
-
+    
+    # Resultado atualizado
     print("Resultado atual:", resultado_atual)
